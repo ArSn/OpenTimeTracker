af320ad3a5c44abe405b88e61a402b0832f84ab1
ArSn <koljaz@web.de>
2016-04-30 21:44:34 +0200

    Added link to github repository on about page and reformatted code.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,15 +12,17 @@
 							<small>What is Open Time Tracker?</small>
 						</h1>
 						<p>Open Time Tracker is a very simple tool to track your working hours. It arose by the feel
-						of <a href="https://github.com/ArSn">one lazy web developer</a> to track his working hours in
-						a browser and have the times in an online database.</p>
+							of <a href="https://github.com/ArSn">one lazy web developer</a> to track his working hours
+							in a browser and have the times in an online database.</p>
 						<p>Right now the system is not even very feature-rich, but the beforementioned lazy developer
-						promised to add a few more features over time. However the page will never become overloaded
-						with a lot of features; as this was one of the main goals of this project: Simplicity to do one
-						thing and one thing only, and doing that thing just right.</p>
+							promised to add a few more features over time. However the page will never become overloaded
+							with a lot of features; as this was one of the main goals of this project: Simplicity to do
+							one thing and one thing only, and doing that thing just right.</p>
 						<p>If you are a developer yourself (or know any), feel free to contribute and/or open feature
-						requests that will kindly be considered: GITHUB_URL_SHOULD_GO_HERE<br />Well actually, feel free
-						to do so even if you are not a developer! Don't be shy, we won't bite.</p>
+							requests that will kindly be considered:
+							<a href="https://github.com/ArSn/OpenTimeTracker">https://github.com/ArSn/OpenTimeTracker</a>
+							<br/>Well actually, feel free to do so even if you are not a developer! Don't be shy, we
+							won't bite.</p>
 						<p>So long, and thanks for all the fish!</p>
 					</div>
 				</div>
